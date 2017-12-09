08e744ec6d311144b01bcf18b8b8d865d48d458a
yop2yop <dark.hoz@gmail.com>
2017-12-09 16:35:59 -0300

    primer csv con porotos negros

--- csv/Untitled.py
+++ csv/Untitled.py
@@ -27,17 +27,31 @@ print '----------------------3'
 print '----------------------3'
 print '----------------------3'
 print '----------------------3'
+print estructura2[0][0][0][0][0][0][0][0][0][0][0]
+b=estructura2[0][0][0][0][0][0][0][0][0][0]
 
 
+lista=estructura2[0][0][0][0][0][0][0][0][0][0]
 
 with open('names.csv', 'w') as csvfile:
-    fieldnames = ['first_name', 'last_name']
+    fieldnames = ['fechas', 'porotos negros']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     writer.writeheader()
-    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-with open('intento1.csv', 'w') as csvfile:
-    writer2 = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer2.writerows(estructura2)
+    for i in range(len(lista)):
+        writer.writerow({'fechas': lista[i][0][0], 'porotos negros': 'BlackBeans'})
+
+###with open('some.csv', 'wb') as f:
+##    writer = csv.writer(f)
+##    writer.writerows(b)
+##    writer.writerows(b)
+
+
+########ES UN VERDADERO EJEMPLO DE COMO HACER UN .CSV ## pajeramente xd
+###with open('names.csv', 'w') as csvfile:
+###    fieldnames = ['first_name', 'last_name']
+###    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+###
+###    writer.writeheader()
+###    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+###    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+###    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
