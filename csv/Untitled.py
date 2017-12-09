67f2a133992e25a16debf859bb8a2c0a87497a9d
yop2yop <dark.hoz@gmail.com>
2017-12-09 17:02:21 -0300

    por primera vez
    c:

--- csv/Untitled.py
+++ csv/Untitled.py
@@ -4,46 +4,41 @@ a='Estaciones_UChile.mat'
 archive= sio.loadmat(a)
 sio.whosmat(a)
 print archive
-
-print '----------------------1'
-print '----------------------1'
-print '----------------------1'
-print '----------------------1'
-
+#### ACA SE NOS METEMOS EN ESTRUCTURAS 
 estructura1 = archive['Estaciones_UChile']
-print estructura1
-print '----------------------2'
-print '----------------------2'
-print '----------------------2'
-print '----------------------2'
-
-
 estructura2 = estructura1['Minutos']
-print estructura2
 ###archive.Dias{1, 1}.Datos_diario
 ### Estaciones_UChile.Minutos.Estacion_UChile_2017{1, 1}.Datos
 
-print '----------------------3'
-print '----------------------3'
-print '----------------------3'
-print '----------------------3'
-print estructura2[0][0][0][0][0][0][0][0][0][0][0]
-b=estructura2[0][0][0][0][0][0][0][0][0][0]
+## ACA ME METO EN LISTAS  y esas cosas
+lista=estructura2[0][0][0][0][0][0][0][0][0]
 
+##lista[0] = TIMESTAMP (Son las fechas) 
+##lista[1] = RECORS    (es el numero de caption tomados)
+##lista[2] = BattV_min
+##lista[3] = PTemp_C_Avg
+##lista[4] = Pres_mbar_Avg
+##lista[5] = Temp_C_Avg
+##lista[6] = Temp_C_Max
+##lista[7] = Temp_C_Min
+##lista[8] = RH
+##lista[9] = Slr_Wm2_Avg (Esta lleno de NAN eso)
+##lista[10] = total_flu_langl... (Esta lleno de NAN eso)
+##lista[11] =
+##lista[12] =
+##lista[13] =
+##lista[14] =
 
-lista=estructura2[0][0][0][0][0][0][0][0][0][0]
-
-with open('names.csv', 'w') as csvfile:
-    fieldnames = ['fechas', 'porotos negros']
+#### BattV_min  
+nombre='BattV_Min'
+with open(nombre + '.csv', 'w') as csvfile:
+    fieldnames = ['Fechas', nombre ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    for i in range(len(lista)):
-        writer.writerow({'fechas': lista[i][0][0], 'porotos negros': 'BlackBeans'})
+    for i in range(len(lista[0])): ## LEN de todos los datos de fechas
+        writer.writerow({'Fechas': lista[0][i][0][0], nombre : lista[2][i][0]})
+####
 
-###with open('some.csv', 'wb') as f:
-##    writer = csv.writer(f)
-##    writer.writerows(b)
-##    writer.writerows(b)
 
 
 ########ES UN VERDADERO EJEMPLO DE COMO HACER UN .CSV ## pajeramente xd
