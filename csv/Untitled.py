4c9d43ff687ec913e34240ec1ac7f0f093028c78
yop2yop <dark.hoz@gmail.com>
2017-12-10 16:15:53 -0300

    TodosHoras.Csv

--- csv/Untitled.py
+++ csv/Untitled.py
@@ -31,7 +31,7 @@ lista=estructura2[0][0][0][0][0][0][0][0][0]
 
 #### BattV_min  
 nombre='BattV_Min'
-with open(nombre + '.csv', 'w') as csvfile:
+with open(nombre + 'minutos'+ '.csv', 'w') as csvfile:
     fieldnames = ['Fechas', nombre ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
@@ -40,7 +40,7 @@ with open(nombre + '.csv', 'w') as csvfile:
 ####
 #### PTemp_C_Avg 
 nombre='PTemp_C_Avg'
-with open(nombre + '.csv', 'w') as csvfile:
+with open(nombre + 'minutos'+ '.csv', 'w') as csvfile:
     fieldnames = ['Fechas', nombre ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
@@ -70,9 +70,19 @@ lista2 =estructura3[0][0][0][0][0][0]
 print '-------------------'
 for j in range(18):
     nombre= str(lista2[2][j]).replace(" ", "")
-    with open(nombre + '.csv', 'w') as csvfile:
+    with open(nombre +'.csv', 'w') as csvfile:
         fieldnames = ['Fechas', nombre ]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for i in range(len(lista2[0])): ## LEN de todos los datos de fechas
             writer.writerow({'Fechas': lista2[0][i][0][0], nombre : lista2[1][i][j]})
+
+################## Todos
+nombre= 'TodosHoras'
+with open(nombre + '.csv', 'w') as csvfile:
+    fieldnames = ['Fechas', str(lista2[2][0]).replace(" ", ""), str(lista2[2][1]).replace(" ", ""),str(lista2[2][2]).replace(" ", ""),str(lista2[2][3]).replace(" ", ""),str(lista2[2][4]).replace(" ", ""),str(lista2[2][5]).replace(" ", ""),str(lista2[2][6]).replace(" ", ""),str(lista2[2][7]).replace(" ", ""),str(lista2[2][8]).replace(" ", ""),str(lista2[2][9]).replace(" ", ""),str(lista2[2][10]).replace(" ", ""),str(lista2[2][11]).replace(" ", ""),str(lista2[2][12]).replace(" ", ""),str(lista2[2][13]).replace(" ", ""),str(lista2[2][14]).replace(" ", ""),str(lista2[2][15]).replace(" ", ""),str(lista2[2][16]).replace(" ", ""),str(lista2[2][17]).replace(" ", ""),str(lista2[2][18]).replace(" ", "")]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for i in range(len(lista2[0])): ## LEN de todos los datos de fechas
+        writer.writerow({'Fechas': lista2[0][i][0][0],str(lista2[2][0]).replace(" ", "") : lista2[1][i][0] ,str(lista2[2][1]).replace(" ", "") : lista2[1][i][1] ,str(lista2[2][2]).replace(" ", "") : lista2[1][i][2] ,str(lista2[2][3]).replace(" ", "") : lista2[1][i][3] ,str(lista2[2][4]).replace(" ", "") : lista2[1][i][4] ,str(lista2[2][5]).replace(" ", "") : lista2[1][i][5] ,str(lista2[2][6]).replace(" ", "") : lista2[1][i][6] ,str(lista2[2][7]).replace(" ", "") : lista2[1][i][7] ,str(lista2[2][8]).replace(" ", "") : lista2[1][i][8] ,str(lista2[2][9]).replace(" ", "") : lista2[1][i][9] ,str(lista2[2][10]).replace(" ", "") : lista2[1][i][10] ,str(lista2[2][11]).replace(" ", "") : lista2[1][i][11] ,str(lista2[2][12]).replace(" ", "") : lista2[1][i][12] ,str(lista2[2][13]).replace(" ", "") : lista2[1][i][13] ,str(lista2[2][14]).replace(" ", "") : lista2[1][i][14] ,str(lista2[2][15]).replace(" ", "") : lista2[1][i][15] ,str(lista2[2][16]).replace(" ", "") : lista2[1][i][16] ,str(lista2[2][17]).replace(" ", "") : lista2[1][i][17] ,str(lista2[2][18]).replace(" ", "")  : lista2[1][i][18]})
+
