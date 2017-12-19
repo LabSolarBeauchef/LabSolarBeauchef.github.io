6b1263c1f6fece114703d6f05f2f5c4be62d02ac
yop2yop <dark.hoz@gmail.com>
2017-12-19 01:00:53 -0300

    listo los csv

--- Untitled.py
+++ Untitled.py
@@ -1,6 +1,8 @@
 import scipy.io as sio
 import csv
+import time
 
+tiempo1=time.time()
 a='Estaciones_UChile.mat'
 archive= sio.loadmat(a)
 sio.whosmat(a)
@@ -28,6 +30,7 @@ lista3=estructura2[0][0][0][0][1][0][0][0][0]
 ##lista[12] = WS_ms_S_WVT
 ##lista[13] = WindDir_d1_WVT
 ##lista[14] = WindDir_SD1_WVT
+print '-------- Imprimir por minutos-----------'
 nombres=['Fechas','RECORS','BattV_min','PTemp_C_Avg','Pres_mbar_Avg','Temp_C_Avg','Temp_C_Max','Temp_C_Min','RH','Slr_Wm2_Avg','Total_flu','Rain_mm_Tot','WS_ms_S_WVT','WindDir_d1_WVT','WindDir_SD1_WVT']
 #### BattV_min  
 for j in range(2,len(nombres)):
@@ -47,8 +50,26 @@ with open('./csv/minutos/' + nombre +'_minutos'+'.csv', 'w') as csvfile:
     for i in range(len(lista[0])): ## LEN de todos los datos de fechas
            writer.writerow({'Fechas': lista[0][i][0][0],  nombres[5]: lista[5][i][0] ,nombres[6]: lista[6][i][0],nombres[7]: lista[7][i][0] })
 
-
-
+print lista3
+for j in range(2,len(lista3[1])):
+    nombre= ((str(lista3[1][j]).replace(" ", "")).replace("u'", "")).replace("'", "") #str(lista2[2][j]).replace(" ", "")
+    with open('./csv/minutos/'+nombre +'_minutos'+'.csv', 'w') as csvfile:
+        fieldnames = ['Fechas', nombre ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(len(lista[0])): ## LEN de todos los datos de fechas
+            writer.writerow({'Fechas': lista3[0][1][0][0],  nombre : str(lista3[2][i][j]).replace("[u'NAN']", "0")})
+ 
+nombre= 'Datos_radiacion'
+with open('./csv/minutos/'+nombre +'_minutos'+'.csv', 'w') as csvfile:
+    fieldnames = ['Fechas', ((str(lista3[1][2]).replace(" ", "")).replace("u'", "")).replace("'", ""), ((str(lista3[1][3]).replace(" ", "")).replace("u'", "")).replace("'", ""),((str(lista3[1][4]).replace(" ", "")).replace("u'", "")).replace("'", "")  ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for i in range(len(lista[0])): ## LEN de todos los datos de fechas
+        writer.writerow({'Fechas': lista3[0][1][0][0],  ((str(lista3[1][2]).replace(" ", "")).replace("u'", "")).replace("'", "")  : str(lista3[2][i][2]).replace("[u'NAN']", "0"), ((str(lista3[1][3]).replace(" ", "")).replace("u'", "")).replace("'", "")  : str(lista3[2][i][3]).replace("[u'NAN']", "0"),   ((str(lista3[1][4]).replace(" ", "")).replace("u'", "")).replace("'", "")  : str(lista3[2][i][4]).replace("[u'NAN']", "0"), })
+ 
+tiempo2=time.time()
+print 'Me demoro ' + str(tiempo2-tiempo1) + 'segundos'
 print '-------- Imprimir por hora-----------'
 estructura3 = estructura1['Horas']
 lista2 =estructura3[0][0][0][0][0][0]
@@ -86,6 +107,8 @@ with open('./csv/hora/' + nombre +'_hora'+'.csv', 'w') as csvfile:
     for i in range(len(lista2[0])): ## LEN de todos los datos de fechas
         writer.writerow({'Fechas': lista2[0][i][0][0],str(lista2[2][0]).replace(" ", "") : lista2[1][i][0] ,str(lista2[2][1]).replace(" ", "") : lista2[1][i][1] ,str(lista2[2][2]).replace(" ", "") : lista2[1][i][2] ,str(lista2[2][3]).replace(" ", "") : lista2[1][i][3] ,str(lista2[2][4]).replace(" ", "") : lista2[1][i][4] ,str(lista2[2][5]).replace(" ", "") : lista2[1][i][5] ,str(lista2[2][6]).replace(" ", "") : lista2[1][i][6] ,str(lista2[2][7]).replace(" ", "") : lista2[1][i][7] ,str(lista2[2][8]).replace(" ", "") : lista2[1][i][8] ,str(lista2[2][9]).replace(" ", "") : lista2[1][i][9] ,str(lista2[2][10]).replace(" ", "") : lista2[1][i][10] ,str(lista2[2][11]).replace(" ", "") : lista2[1][i][11] ,str(lista2[2][12]).replace(" ", "") : lista2[1][i][12] ,str(lista2[2][13]).replace(" ", "") : lista2[1][i][13] ,str(lista2[2][14]).replace(" ", "") : lista2[1][i][14] ,str(lista2[2][15]).replace(" ", "") : lista2[1][i][15] ,str(lista2[2][16]).replace(" ", "") : lista2[1][i][16] ,str(lista2[2][17]).replace(" ", "") : lista2[1][i][17] ,str(lista2[2][18]).replace(" ", "")  : lista2[1][i][18]})
 
+tiempo3=time.time()
+print 'Me demoro ' + str(tiempo3-tiempo2) + 'segundos'
 ################ DIAS
 print '-------- Imprimir por dias-----------'
 estructura3 = estructura1['Dias']
@@ -122,3 +145,6 @@ with open('./csv/dias/' + nombre +'_dias'+'.csv', 'w') as csvfile:
     writer.writeheader()
     for i in range(len(lista2[0])): ## LEN de todos los datos de fechas
         writer.writerow({'Fechas': lista2[0][i][0][0],str(lista2[2][0]).replace(" ", "") : lista2[1][i][0] ,str(lista2[2][1]).replace(" ", "") : lista2[1][i][1] ,str(lista2[2][2]).replace(" ", "") : lista2[1][i][2] ,str(lista2[2][3]).replace(" ", "") : lista2[1][i][3] ,str(lista2[2][4]).replace(" ", "") : lista2[1][i][4] ,str(lista2[2][5]).replace(" ", "") : lista2[1][i][5] ,str(lista2[2][6]).replace(" ", "") : lista2[1][i][6] ,str(lista2[2][7]).replace(" ", "") : lista2[1][i][7] ,str(lista2[2][8]).replace(" ", "") : lista2[1][i][8] ,str(lista2[2][9]).replace(" ", "") : lista2[1][i][9] ,str(lista2[2][10]).replace(" ", "") : lista2[1][i][10] ,str(lista2[2][11]).replace(" ", "") : lista2[1][i][11] ,str(lista2[2][12]).replace(" ", "") : lista2[1][i][12] ,str(lista2[2][13]).replace(" ", "") : lista2[1][i][13] ,str(lista2[2][14]).replace(" ", "") : lista2[1][i][14] ,str(lista2[2][15]).replace(" ", "") : lista2[1][i][15] ,str(lista2[2][16]).replace(" ", "") : lista2[1][i][16] ,str(lista2[2][17]).replace(" ", "") : lista2[1][i][17] ,str(lista2[2][18]).replace(" ", "")  : lista2[1][i][18]})
+        
+tiempo4=time.time()
+print 'Me demoro ' + str(tiempo4-tiempo3) + 'segundos'
